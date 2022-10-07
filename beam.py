43d6fe71c22ab4de6e5826cf36a382ccdb386338
Hellman Oded <oded.hellman@gmail.com>
2022-10-07 17:44:53 +0300

    add split class

--- beam.py
+++ beam.py
@@ -1,9 +1,10 @@
+import logging
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
 INPUT_FILE = "./data/snp500source.csv"
 OUTPUT_FILE = "./output/result.txt"
-
+HEADER = 'Date,Open,High,Low,Close,Volume'
 class SnPOptions(PipelineOptions):
 
   @classmethod
@@ -12,4 +13,12 @@ class SnPOptions(PipelineOptions):
                         help='Input file')
     parser.add_argument('--output', default='OUTPUT_FILE',
                         help='Output file')
-    
+
+class Split(beam.DoFn):
+    def process(self, element):
+        date, open, _, _, close, _ = element.split(',')
+        return [{
+            'date': date,
+            'open': float(open),
+            'close': float(close)
+        }]
