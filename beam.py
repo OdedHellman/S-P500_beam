1995fb385ca261811fa562d331df39f692c7b0b6
Hellman Oded <oded.hellman@gmail.com>
2022-10-07 15:23:28 +0300

    add options class

--- beam.py
+++ beam.py
@@ -4,3 +4,12 @@ from apache_beam.options.pipeline_options import PipelineOptions
 INPUT_FILE = "./data/snp500source.csv"
 OUTPUT_FILE = "./output/result.txt"
 
+class SnPOptions(PipelineOptions):
+
+  @classmethod
+  def _add_argparse_args(cls, parser):
+    parser.add_argument('--input', default='INPUT_FILE',
+                        help='Input file')
+    parser.add_argument('--output', default='OUTPUT_FILE',
+                        help='Output file')
+    
